b31e169800723f041662b54cfc841f0995b1b222
23101123130014 <jacobmwansambe1@gmail.com>
2025-01-02 22:19:31 +0300

    Create mutiplication table.cpp

---
+++ mutiplication table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x;
+	int n;
+	cout<<" enter the number to generate multiplication table for it ";
+	cin>>x;
+	for(int n = 1; n<=10; n++){
+
+	
+	cout<<endl;
+	cout<<x <<" * " << n <<" = "<< x *n ;
+}
+}
